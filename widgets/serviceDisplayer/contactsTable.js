2f7c5b060a76e2004d0b770dede043cdc66c21c9
me <me@simpo.org>
2012-12-04 09:34:21 +0000

    * Refactored to make the comparisions more robust.  * Added styling to make on-screen presenation better.

--- widgets/serviceDisplayer/contactsTable.js
+++ widgets/serviceDisplayer/contactsTable.js
@@ -19,7 +19,7 @@ define([
 ], function(
 	declare, _widget, _templated, _wTemplate, i18n, strings, template,
 	array, domConstr, lang, require
-) {
+){
 	"use strict";
 	
 	var construct = declare([_widget, _templated, _wTemplate], {
@@ -32,6 +32,7 @@ define([
 		"templateString": template,
 		
 		"data": [],
+		"title": "",
 		
 		postCreate: function(){
 			this._init();
@@ -43,9 +44,32 @@ define([
 		
 		_processContacts: function(){
 			array.forEach(this.data, function(contact, n){
-				var tr = domConstr.create("tr", {}, this.domNode);
+				var tr = domConstr.create("tr", {}, this.tableNode);
 				this._processContact(contact, tr);
 			}, this);
+			if(!this._isBlank(this.data)){
+				this._writeLastRow();
+			}
+			if(!this._isBlank(this.title)){
+				this._addTitle();
+			}
+		},
+		
+		_addTitle: function(){
+			domConstr.create("h2",{
+				"innerHTML": this.title + ":",
+			}, this.domNode, "first");
+		},
+		
+		_writeLastRow: function(){
+			var tr = domConstr.create("tr", {}, this.tableNode);
+			domConstr.create("td", {
+				"innerHTML": "&nbsp;",
+				"class": "r"
+			}, tr);
+			domConstr.create("td", {
+				"innerHTML": "&nbsp;",
+			}, tr);
 		},
 		
 		_processContact: function(contact, tr){
@@ -60,10 +84,37 @@ define([
 		},
 		
 		_camelize: function(str){
-			return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-				if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+			return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index){
+				if(+match === 0){
+					return ""; // or if (/\s+/.test(match)) for white spaces
+				}
 				return index == 0 ? match.toLowerCase() : match.toUpperCase();
 			});
+		},
+		
+		_isBlank: function(value){
+			if((value === null) || (value === undefined) || (value === "")){
+				return true;
+			}
+			
+			if(toString.call(value) === '[object String]'){
+				if(lang.trim(value) === ""){
+					return true;
+				}
+			}else if(Object.prototype.toString.call(value) === '[object Object]'){
+				for(var key in map){
+					if(map.hasOwnProperty(key)){
+						return false;
+					}
+				}
+				return true;
+			}else if(Object.prototype.toString.call(value) === '[object Array]'){
+				if(value.length == 0){
+					return true;
+				}
+			}
+			
+			return false;
 		}
 	});
 	
