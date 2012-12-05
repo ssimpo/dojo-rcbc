4934ce4c4e5867f01ce129f97a2afcf6e5c6480d
me <me@simpo.org>
2012-12-05 13:47:34 +0000

    Added the i18n strings.

--- widgets/serviceDisplayer/nls/accessTable.js
+++ widgets/serviceDisplayer/nls/accessTable.js
@@ -1,5 +1,18 @@
 define({
 	root: ({
-		
+		"referrelOnly": "By referral only",
+		"appointmentOnly": "By appointnent only",
+		"dropinDetails": "Dropin details",
+		"ageTarget": "Age target",
+		"otherRestrictions": "Other restrictions",
+		"above": "above",
+		"below": "below",
+		"between": "between",
+		"restrictedToMen": "This is targetted towards men",
+		"restrictedToWomen": "This is targetted towards women",
+		"restrictedToAreas": "Restricted to the following areas",
+		"and": " and ",
+		"yrs": "yrs",
+		"mths": "mths"
 	})
 });
