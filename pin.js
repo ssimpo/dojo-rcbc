2b241b7a93b6c369a157663c0424431a8d9a0517
Stephen Simpson <me@simpo.org>
2012-12-10 11:26:30 +0000

    Moved the viewers and menu into an app style widget (controller) and moved entire project into its own project file.

---
+++ pin.js
@@ -0,0 +1,44 @@
+// summary:
+//
+// description:
+//
+// author:
+//		Stephen Simpson <me@simpo.org>, <http://simpo.org>
+define([
+	"dojo/_base/declare",
+	"dijit/_WidgetBase",
+	"dijit/_TemplatedMixin",
+	"dijit/_WidgetsInTemplateMixin",
+	"dojo/i18n",
+	"dojo/i18n!./nls/pin",
+	"dojo/text!./views/pin.html",
+	
+	
+	"./pin/sideMenu",
+	"./pin/serviceDisplayer"
+], function(
+	declare, _widget, _templated, _wTemplate, i18n, strings, template
+) {
+	"use strict";
+	
+	var construct = declare([_widget, _templated, _wTemplate], {
+		// i18n: object
+		//		The internationalisation text-strings for current browser language.
+		"i18n": strings,
+		
+		// templateString: string
+		//		The loaded template string containing the HTML formatted template for this widget.
+		"templateString": template,
+		
+		
+		postCreate: function(){
+			this._init();
+		},
+		
+		_init: function(){
+			
+		}
+	});
+	
+	return construct;
+});
