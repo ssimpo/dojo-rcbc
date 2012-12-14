2e55bb02dcd7fad7beb8d4ce5520fc421afa37fa
Stephen Simpson <me@simpo.org>
2012-12-14 16:38:56 +0000

    Added try/catch clauses to try and find source of error when page is refreshed while showing the shortlist.

--- pin/shortlist.js
+++ pin/shortlist.js
@@ -49,45 +49,67 @@ define([
 			"title": true,
 			"description": true,
 			"keyFeatures": true,
-			"contacts": true,
+			"contacts": false,
 			"costs": true,
 			"venues": true,
 			"serviceHours": true,
 			"accessDetails": true
 		},
 		
+		_init: function(){
+			try{
+				this._initNodes();
+				this._destroyDisplayers();
+				domConstr.empty(this.domNode);
+			}catch(e){
+				console.info("Could not ininitate shortlist displayer.");
+			}
+		},
+		
 		_initNodes: function(){
-			if(this.application !== null){
-				if(this.parentNode === null){
-					this.parentNode = this.application.articleContentNode;
+			try{
+				if(this.application !== null){
+					if(this.parentNode === null){
+						this.parentNode = this.application.articleContentNode;
+					}
+					if(this.hiddenNode === null){
+						this.hiddenNode = this.application.hiddenDiv;
+					}
 				}
-				if(this.hiddenNode === null){
-					this.hiddenNode = this.application.hiddenDiv;
-				}
+			}catch(e){
+				console.info("Could not ininitate shortlist nodes.");
 			}
 		},
 		
 		_hideWidget: function(){
-			if(this.hiddenNode !== null){
-				domConstr.place(this.domNode, this.hiddenNode);
-				domClass.replace(
-					this.parentNode,
-					"articleContent",
-					"articleContent-wide"
-				);
+			try{
+				if(this.hiddenNode !== null){
+					domConstr.place(this.domNode, this.hiddenNode);
+					domClass.replace(
+						this.parentNode,
+						"articleContent",
+						"articleContent-wide"
+					);
+				}
+			}catch(e){
+				console.info("Could not hide shortlist displayer.");
 			}
 		},
 		
 		_showWidget: function(){
-			if(this.parentNode !== null){
-				domConstr.place(
-					this.domNode, this.parentNode, this.parentPosPlace
-				);
-				domClass.replace(
-					this.parentNode,
-					"articleContent-wide",
-					"articleContent"
-				);
+			try{
+				if(this.parentNode !== null){
+					domConstr.place(
+						this.domNode, this.parentNode, this.parentPosPlace
+					);
+					domClass.replace(
+						this.parentNode,
+						"articleContent-wide",
+						"articleContent"
+					);
+				}
+			}catch(e){
+				console.info("Could not show shortlist displayer.");
 			}
 		},
 		
@@ -96,38 +118,62 @@ define([
 		},
 		
 		_setValueAttr: function(value){
-			this._initNodes();
+			this._init();
 			
-			this._destroyDisplayers();
-			domConstr.empty(this.domNode);
-			if(this._isBlank(value)){
-				this._hideWidget();
-			}else{
-				topic.publish("/rcbc/pin/titleChange", "");
-				this._showWidget();
-				this._displayShortlist(value);
+			try{
+				if(this._isBlank(value)){
+					this._hideWidget();
+				}else{
+					topic.publish("/rcbc/pin/titleChange", "");
+					this._showWidget();
+					this._displayShortlist(value);
+				}
+				this.value = value;
+			}catch(e){
+				console.info("Could not set shortlist displayer value.");
 			}
-			this.value = value;
 		},
 		
 		_destroyDisplayers: function(){
-			array.forEach(this._displayers, function(displayer){
-				displayer.destroy();
-			}, this);
-			this._displayers = new Array();
+			try{
+				array.forEach(this._displayers, function(displayer){
+					displayer.destroy();
+				}, this);
+				this._displayers = new Array();
+			}catch(e){
+				console.info("Could not destroy shortlist displayers");
+			}
 		},
 		
 		_addHr: function(){
-			domConstr.create("br", null, this.domNode);
-			domConstr.create("hr", null, this.domNode);
-			domConstr.create("br", null, this.domNode);
+			try{
+				domConstr.create("br", null, this.domNode);
+				domConstr.create("hr", null, this.domNode);
+				domConstr.create("br", null, this.domNode);
+			}catch(e){
+				console.info("Could not add shortlist divider");
+			}
 		},
 		
 		_displayShortlist: function(ids){
-			this._displayers = new Array();
-			var services = this._getServices(ids);
+			try{
+				this._displayers = new Array();
+				var services = this._getServices(ids);
 			
-			array.forEach(services, function(service, n){
+				array.forEach(services, function(service, n){
+					this._createDisplayer(service.data);
+				
+					if(n < (services.length-1)){
+						this._addHr();
+					}
+				}, this);
+			}catch(e){
+				console.info("Could not display shortlist.");
+			}
+		},
+		
+		_createDisplayer: function(data){
+			try{
 				var displayer = new serviceDisplayer({
 					"application": this.application,
 					"parentNode": this.domNode,
@@ -136,23 +182,25 @@ define([
 				});
 				this._displayers.push(displayer);
 				domConstr.place(displayer.domNode, this.domNode);
-				displayer.set("value", service.data);
-				
-				if(n < (services.length-1)){
-					this._addHr();
-				}
-			}, this);
+				displayer.set("value", data);
+			}catch(e){
+				console.info("Could not create displayer.");
+			}
 		},
 		
 		_getServices: function(ids){
 			var services = new Array();
 			
-			array.forEach(ids, function(id){
-				var service = this.application.store.get(id);
-				if(!this._isBlank(service)){
-					services.push(service);
-				}
-			}, this);
+			try{
+				array.forEach(ids, function(id){
+					var service = this.application.store.get(id);
+					if(!this._isBlank(service)){
+						services.push(service);
+					}
+				}, this);
+			}catch(e){
+				console.info("Could not get services");
+			}
 			
 			return services;
 		}
