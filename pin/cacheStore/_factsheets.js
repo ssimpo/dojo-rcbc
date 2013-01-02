cf09715b8d3a241bc1f6429a1365e22c62ee7204
Stephen Simpson <me@simpo.org>
2013-01-02 15:32:57 +0000

    Added _info and _factsheets as stub mixin classes.  Moved more of the service and venue content to their respective mixins.

---
+++ pin/cacheStore/_factsheets.js
@@ -0,0 +1,32 @@
+// summary:
+//
+// description:
+//
+// author:
+//		Stephen Simpson <me@simpo.org>, <http://simpo.org>
+define([
+	"dojo/_base/declare",
+	"dojo/_base/array",
+	"dojo/_base/lang",
+	"dojo/topic"
+], function(
+	declare, array, lang, request, topic
+) {
+	"use strict";
+	
+	var construct = declare(null, {
+		"_factsheetCache": [],
+		
+		constructor: function(){
+			this.addIntervalCheck(function(){
+				if(!this._isBlank(this._factsheetCache)){
+					this.addIntervalCommand(
+						lang.hitch(this, this._updateFromFactsheetCache)
+					);
+				}
+			});
+		}
+	});
+	
+	return construct;
+});
---
+++ pin/cacheStore/_info.js
@@ -0,0 +1,32 @@
+// summary:
+//
+// description:
+//
+// author:
+//		Stephen Simpson <me@simpo.org>, <http://simpo.org>
+define([
+	"dojo/_base/declare",
+	"dojo/_base/array",
+	"dojo/_base/lang",
+	"dojo/topic"
+], function(
+	declare, array, lang, request, topic
+) {
+	"use strict";
+	
+	var construct = declare(null, {
+		"_infoCache": [],
+		
+		constructor: function(){
+			this.addIntervalCheck(function(){
+				if(!this._isBlank(this._infoCache)){
+					this.addIntervalCommand(
+						lang.hitch(this, this._updateFromInfoCache)
+					);
+				}
+			});
+		}
+	});
+	
+	return construct;
+});
