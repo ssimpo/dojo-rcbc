6e033f300f2a9f6db02c9dfc198c48b6e2d0fe3c
Stephen Simpson <me@simpo.org>
2013-06-18 07:37:55 +0100

    Moved creation of the map to the actual code and out of the template to avoid errors with trying to place the map before it is loaded.

--- pin/serviceDisplayer/venuesDisplayer.js
+++ pin/serviceDisplayer/venuesDisplayer.js
@@ -19,11 +19,11 @@ define([
 	"dojo/topic",
 	"./venuesDisplayer/venueDisplayer",
 	"dojo/on",
-	
-	"simpo/maps/google/canvas"
+	"simpo/maps/google/canvas",
+	"simpo/interval"
 ], function(
 	declare, _widget, _templated, _wTemplate, i18n, strings, template,
-	typeTest, domConstr, lang, array, topic, venueDisplayer, on
+	typeTest, domConstr, lang, array, topic, venueDisplayer, on, canvas, interval
 ){
 	"use strict";
 	
@@ -43,6 +43,7 @@ define([
 		"application": {},
 		"titleLevel": 2,
 		"hiddenNode": null,
+		"mapWidget": null,
 		"value": null,
 		"_venueIds": {},
 		
@@ -50,13 +51,12 @@ define([
 			"map": true
 		},
 		
-		
-		postCreate: function(){
-			this._init();
+		constructor: function(){
+			this._initTopicSubscriptions();
 		},
 		
-		_init: function(){
-			this._initTopicSubscriptions();
+		postCreate: function(){
+			this._initMapWidget();
 		},
 		
 		_initTopicSubscriptions: function(){
@@ -66,44 +66,65 @@ define([
 			);
 		},
 		
+		_initMapWidget: function(){
+			var mapWidget = new canvas({
+				"class": "rcbcWidgetsVenueDisplayerMap",
+				"callback": lang.hitch(this, function(){
+					this.mapWidget = mapWidget
+					this._hideMap();
+				})
+			});
+		},
+		
 		_setValueAttr: function(value){
 			try{
 				this.clear();
-				this._hideMap();
+				this._hideMap(); // ???? Hide each time?
+				
 				if(!typeTest.isEmpty(value)){
-					value = this._parseVenuesArray(value);
-					this.value = value;
+					value = this._parseVenueData(value);
 					this._addVenues(value);
 				}else{
-					this.value = new Array();
+					value = new Array();
 				}
+				
+				this.value = value;
 			}catch(e){
 				console.info("Could not set the venues displayer value.", e, value);
 			}
 		},
 		
-		_parseVenuesArray: function(venues){
+		_parseVenueData: function(venues){
 			var newVenues = new Array();
 			
 			try{
 				if(typeTest.isArray(venues)){
-					array.forEach(venues, function(venue, n){
-						if(typeTest.isString(venue)){
-							newVenues.push({
-								"description": "",
-								"venueId": venue
-							});
-						}else if(this._isValueVenueObject(venue)){
-							newVenues.push(venue);
-						}
-					}, this);
+					newVenues = this._parseVenuesArray(venues);
 				}else if(typeTest.isObject(venues)){
 					if(this._isValueVenueObject(venues)){
 						newVenues.push(venues);
 					}
 				}
 			}catch(e){
-				console.info("Could not parse venues array: ", venues);
+				console.info("Could not parse venues data: ", venues, e);
+			}
+			
+			return newVenues;
+		},
+		
+		_parseVenuesArray: function(venues){
+			var newVenues = new Array();
+			
+			try{
+				array.forEach(venues, function(venue, n){
+					if(typeTest.isString(venue)){
+						newVenues.push({"description": "", "venueId": venue});
+					}else if(this._isValueVenueObject(venue)){
+						newVenues.push(venue);
+					}
+				}, this);
+			}catch(e){
+				console.info("Could not parse venues array: ", venues, e);
 			}
 			
 			return newVenues;
@@ -117,78 +138,96 @@ define([
 			return false;
 		},
 		
-		clear: function(){
-			try{
-				this._venueIds = new Object();
-				domConstr.empty(this.venuesNode);
-				if(this.show.map){
-					this.mapNode.clear();
-				}
-			}catch(e){
-				console.info("Could not clear the map.");
-			}
-		},
-		
 		_addVenues: function(venues){
 			array.forEach(venues, function(venue){
-				if(!typeTest.isProperty(this._venueIds, venue.venueId)){
+				var venueId = venue.venueId.toLowerCase();
+				
+				if(!typeTest.isProperty(this._venueIds, venueId)){
 					try{
-						var venueWidget = new venueDisplayer({
-							"application": this.application,
-							"value": venue.venueId,
-							"description": venue.description,
-							"titleLevel": this.titleLevel
-						});
-						domConstr.place(
-							venueWidget.domNode,
-							this.venuesNode
-						);
-						domConstr.place(
-							this.venuesNode,
-							this.domNode
-						);
-					}catch(e){
-						console.info("Could not place venue widget.");
-					}
-					
-					if(this.show.map){
-						try{
-							this._venueIds[venue.venueId.toLowerCase()] = {
-								"widget": venueWidget,
-								"mapMarker": null
-							};
-							this._plotOnMap(venue.venueId);
-						}catch(e){
-							console.info("Could not plot venue: ", venue);
+						var venueWidget = this._createVenueWidget(venue);
+						this._showNode(venueWidget, this.venuesNode);
+						this._showNode(this.venuesNode, this.domNode);
+						
+						if(this.show.map){
+							this._plotOnMap(venueId);
 						}
+					}catch(e){
+						console.info("Could not place venue widget.", e);
 					}
 				}
 			}, this);
 		},
 		
+		_createVenueWidget: function(venue){
+			var venueId = venue.venueId.toLowerCase();
+			var venueWidget = null;
+			
+			try{
+				venueWidget = venueDisplayer({
+					"application": this.application,
+					"value": venueId,
+					"description": venue.description,
+					"titleLevel": this.titleLevel
+				});
+			
+				if(!typeTest.isProperty(venueId)){
+					this._venueIds[venueId] = {
+						"widget": venueWidget, "mapMarker": null
+					};
+				}
+			}catch(e){
+				console.info("Could not create venue widget");
+			}
+			
+			return venueWidget;
+		},
+		
 		_plotOnMap: function(venueId){
-			if(!typeTest.isBlank(venueId)){
-				venueId = venueId.toLowerCase();
-				var data = this.application.store.getVenue(venueId);
-				
-				if(!typeTest.isBlank(data)){
-					var postcode = this._getPostcodeFromVenueData(data);
+			try{
+				if(!typeTest.isBlank(venueId)){
+					venueId = venueId.toLowerCase();
+					var data = this.application.store.getVenue(venueId);
 					
-					if(!typeTest.isBlank(postcode)){
-						this.mapNode.plot(postcode, lang.hitch(this, function(marker){
-							this._venueIds[venueId].mapMarker = marker;
-							marker.setIcon("/images/PINsml.png");
-							this._showMap();
-							this.mapNode.centre(
-								marker.position.lat(),
-								marker.position.lng()
-							);
-						}));
+					if(!typeTest.isBlank(data)){
+						var postcode = this._getPostcodeFromVenueData(data);
+						
+						if(!typeTest.isBlank(postcode)){
+							this._plotOnMap2(venueId, postcode);
+						}
+					}else{
+						this.application.store.updateVenue(venueId);
 					}
-				}else{
-					this.application.store.updateVenue(venueId);
+				}
+			}catch(e){
+				console.info("Could not lookup postcode and plot on map.");
+			}
+		},
+		
+		_plotOnMap2: function(venueId, postcode){
+			if(this.mapWidget === null){
+				interval.add(lang.hitch(this, this._plotOnMap2, venueId, postcode));
+			}else{
+				try{
+					this.mapWidget.addOnload(
+						lang.hitch(this, this._plotOnMap3, venueId, postcode)
+					);
+				}catch(e){
+					console.info(e);
 				}
 			}
+		},
+		
+		_plotOnMap3: function(venueId, postcode){
+			venueId = venueId.toLowerCase();
+			
+			this.mapWidget.plot(postcode, lang.hitch(this, function(marker){
+				this._venueIds[venueId].mapMarker = marker;
+				marker.setIcon("/images/PINsml.png");
+				this._showMap();
+				this.mapWidget.centre(
+					marker.position.lat(), marker.position.lng()
+				);
+			}));
 		},
 		
 		_getPostcodeFromVenueData: function(data){
@@ -203,9 +242,23 @@ define([
 			return "";
 		},
 		
+		clear: function(){
+			try{
+				this._venueIds = new Object();
+				domConstr.empty(this.venuesNode);
+				if((this.show.map) && (this.mapWidget !== null)){
+					this.mapWidget.clear();
+				}
+			}catch(e){
+				console.info("Could not clear the map.", e);
+			}
+		},
+		
 		_hideMap: function(){
 			try{
-				this._hideNode(this.mapNode);
+				if(this.mapWidget !== null){
+					this._hideNode(this.mapWidget);
+				}
 			}catch(e){
 				console.info("Could not hide the map");
 			}
@@ -216,6 +269,7 @@ define([
 				if(typeTest.isWidget(node)){
 					node = node.domNode
 				}
+				
 				if(typeTest.isElement(node)){
 					if(this.hiddenNode !== null){
 						domConstr.place(node, this.hiddenNode);
@@ -228,11 +282,12 @@ define([
 		
 		_showMap: function(){
 			this._showNode(
-				this.mapNode,
-				this.application.serviceDisplayer.domNode
+				this.mapWidget.domNode,
+				this.application.serviceDisplayer.domNode,
+				"last"
 			);
 			if(google){
-				google.maps.event.trigger(this.mapNode.map, "resize");
+				google.maps.event.trigger(this.mapWidget.map, "resize");
 			}
 		},
 		
@@ -244,10 +299,9 @@ define([
 				if(typeTest.isWidget(node)){
 					node = node.domNode
 				}
+				
 				if(typeTest.isElement(node)){
-					if(this.hiddenNode !== null){
-						domConstr.place(node, refNode, position);
-					}
+					domConstr.place(node, refNode, position);
 				}
 			}catch(e){
 				console.info("Could not show venue node item.", e);
@@ -257,7 +311,7 @@ define([
 		_venueDataUpdate: function(data){
 			if(!typeTest.isBlank(data)){
 				array.forEach(this.value, function(venue){
-					if(typeTest.isEqual(venue.venueId, data)){
+					if(typeTest.isEqual(venue.venueId.toLowerCase(), data)){
 						this._plotOnMap(data);
 					}
 				}, this);
