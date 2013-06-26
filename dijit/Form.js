7cb2e8f03269741b28b2b9615a683398b5237c6f
me <me@simpo.org>
2013-06-26 09:26:34 +0100

    Added methods for pre-loading service data from the server for the form.

--- dijit/Form.js
+++ dijit/Form.js
@@ -8,9 +8,15 @@ define([
 	"dojo/ready",
 	"dojo/_base/lang",
 	"simpo/store/local",
-	"lib/md5"
+	"lib/md5",
+	"dojo/io-query",
+	"simpo/typeTest",
+	"simpo/xhrManager",
+	"dojo/_base/array",
+	"dojo/query"
 ], function(
-	declare, i18n, strings, Form, registry, on, ready, lang, memory, md5
+	declare, i18n, strings, Form, registry, on, ready, lang, memory, md5,
+	ioQuery, typeTest, xhrManager, array, $
 ){
 	"use strict";
 	
@@ -21,6 +27,15 @@ define([
 		"encrypt": true,
 		"sessionOnly": true,
 		"useStore": false,
+		
+		_serverDataMapper1: [
+			["appointmentOnly", "appointmentOnly", "appointmentOnlyDetails"],
+			["dropIn", "dropIn", "dropInDetails"],
+			["genderTarget", "genderTarget", "genderTargetType"],
+			["geographicCoverage", "geographicRestriction", "geographicCoverage"],
+			["referralOnly", "referralOnly", "referralOnlyDetails"],
+			["ageTarget", "ageTarget", "ageTargetType"]
+		],
 		
 		constructor: function(args){
 			this._init(args);
@@ -56,14 +71,18 @@ define([
 		},
 		
 		postCreate: function(){
-			if(this.store){
+			var query = this._getQuery();
+			if(typeTest.isProperty(query, "id")){
+				ready(lang.hitch(this, function(){
+					this._getServiceData(query.id);
+				}));
+			}else if(this.store){
 				ready(lang.hitch(this, function(){
 					var qry = this.store.query({"id":"formData"});
 					if(qry.length > 0){
 						this.set("value", qry[0].formData);
 					}
 					
-					//console.log("FORM POSTCREATE: ",qry[0].formData);
 					if(this.useStore){
 						registry.findWidgets(this.domNode).forEach(function(widget){
 							on(widget, "focusout", lang.hitch(this, this._widgetUpdate));
@@ -73,13 +92,189 @@ define([
 			}
 		},
 		
+		_getServiceData: function(id){
+			if(typeTest.isString(id)){
+				if(id.length == 32){		
+					xhrManager.add({
+						"url": "/pin.nsf/getService2?openagent&stub=false",
+						"method": "post",
+						"data": {
+							"id": id
+						}
+					}).then(
+						lang.hitch(this, this._getServiceDataSuccess),
+						lang.hitch(this, function(e){
+							console.info("Failed to get service data", e);
+						})
+					);
+				}
+			}
+		},
+		
+		_getVenueData: function(id){
+			if(typeTest.isString(id)){
+				if(id.length == 32){		
+					xhrManager.add({
+						"url": "/pin.nsf/getVenue?openagent",
+						"method": "post",
+						"data": {
+							"id": id
+						}
+					}).then(
+						lang.hitch(this, this._getVenueDataSuccess),
+						lang.hitch(this, function(e){
+							console.info("Failed to get service data", e);
+						})
+					);
+				}
+			}
+		},
+		
+		_getServiceDataSuccess: function(data){
+			console.log("GOT SERVICE DATA", data);
+			if(!typeTest.isBlank(data)){
+				if(typeTest.isProperty(data, "services")){
+					if(data.services.length > 0){
+						data = this._parseServerData(data.services[0])
+						this.set("value", data);
+					}
+				}
+			}
+		},
+		
+		_getVenueDataSuccess: function(data){
+			console.log("GOT VENUE DATA", data);
+			if(typeTest.isProperty(data, "venues")){
+				if(typeTest.isArray(data.venues)){
+					if(data.venues.length > 0){
+						data = data.venues[0];
+						
+						var venueWidget = this._getWidget("venue");
+						if(venueWidget !== undefined){
+							var id = data.id.toLowerCase();
+							var value = venueWidget.get("value");
+							
+							if(!typeTest.isProperty(value, id)){
+								value[id] = data;
+								venueWidget.set("value", value);
+							}
+						}
+						
+					}
+				}
+			}
+		},
+		
+		_getWidget: function(id){
+			var query = $("#"+id);
+			if(query.length > 0){
+				return registry.byNode(query[0]);
+			}
+			
+			return undefined;
+		},
+		
+		_parseServerData: function(data){
+			array.forEach(
+				["Access", "Venues", "Contacts"],
+				function(parser){
+					data = this["_parseService"+parser](data);
+				},
+				this
+			);
+			
+			return data;
+		},
+		
+		_parseServiceAccess: function(data){
+			array.forEach(this._serverDataMapper1, function(tester){
+				data[tester[0]] = this._getDetailsField(data, tester[1], tester[2]);
+			}, this);
+			
+			return data;
+		},
+		
+		_parseServiceVenues: function(data){
+			if(typeTest.isProperty(data, "venues")){
+				if(typeTest.isArray(data.venues)){
+					array.forEach(data.venues, function(venue){
+						this._getVenueData(venue.venueId.toLowerCase());
+					}, this);
+				}
+			}
+			
+			return data;
+		},
+		
+		_parseServiceContacts: function(data){
+			if(typeTest.isProperty(data, "contacts")){
+				if(typeTest.isArray(data.contacts)){
+					var contactsWidget = this._getWidget("contacts");
+					
+					array.forEach(data.contacts, function(contact){
+						if(contactsWidget !== undefined){
+							var id = "";
+							var value = contactsWidget.get("value");
+							if(!typeTest.isProperty(contact, "id")){
+								id = this._createUnid();
+							}else{
+								id = contact.id.toLowerCase();
+							}
+							
+							if(!typeTest.isProperty(value, id)){
+								value[id] = contact;
+								contact.value = contact.details;
+								contactsWidget.set("value", value);
+							}
+							
+						}
+					}, this);
+					
+				}
+			}
+			
+			return data;
+		},
+		
+		_createUnid: function(){
+			var unid = "";
+			for(var i = 1; i <= 32; i++){
+				var no = Math.floor(Math.random()*15);
+				unid += no.toString(15);
+			}
+			return unid.toLowerCase();
+		},
+		
+		_getDetailsField: function(data, testField, detailField){
+			if(typeTest.isProperty(data, testField)){
+				if(typeTest.isProperty(data, detailField)){
+					if (typeTest.isEqual(data[testField], "yes")){
+						return data[detailField];
+					}
+				}
+			}
+			
+			return false;
+		},
+		
+		_getQuery: function(query){
+			query = ((query === undefined) ? location.search : query);
+			
+			if(query !== undefined){
+				query = query.toLowerCase().split(/[#\?]/g);
+				query = (((query[0] === "") & (query.length > 1)) ? query[1] : query[0]);
+				return ioQuery.queryToObject(query);
+			}else{
+				return new Object();
+			}
+		},
+		
 		_hasProperty: function(obj, propName){
 			return Object.prototype.hasOwnProperty.call(obj, propName);
 		},
 		
 		_widgetUpdate: function(event){
 			if(this.store){
-				//console.log("BLUR: ",this.get("value"));
 				if(this.useStore){
 					this.store.put({"id":"formData", "formData":this.get("value")});
 				}
